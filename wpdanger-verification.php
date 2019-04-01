36a00dc29f11b8f478c228696bbb90e299b94ae7
Javier Casares <javier@casares.org>
2019-04-01 11:27:25 +0200

    fix

--- wpdanger-verification.php
+++ wpdanger-verification.php
@@ -16,61 +16,53 @@ License URI: https://eupl.eu/1.2/en/
 Text Domain: wpdanger-verification
 */
 defined('ABSPATH') or die('Bye bye!');
-if ( !class_exists('wpdanger_verification') )
+
+function wpdanger_verification_show()
 {
-	function wpdanger_verification()
+	global $post;
+	if( !is_404() )
 	{
-		// nothing to do here!
-	}
-	function wpdanger_verification_show()
-	{
-		global $post;
-		if( !is_404() )
+		$ok = false;
+		$verification_code = null;
+		if( is_home() || is_front_page() )
 		{
-			$ok = false;
-			$verification_code = null;
-			if( is_home() || is_front_page() )
+			$verification_code = get_option( 'wpdanger_verification_code' );
+			if($verification_code)
 			{
-				$verification_code = get_option( 'wpdanger_verification_code' );
-				if($verification_code)
-				{
-					$ok = true;
-				}
+				$ok = true;
 			}
-			if ( $ok )
-			{
-				echo '<meta name="wpdanger" content="' . wp_kses($verification_code, array(), array()) .'">' . "\n";
-			}
-			unset($ok, $verification_code);
 		}
-	}	
-	$wpdanger_verification_meta = wpdanger_verification();
-	if (isset($wpdanger_verification_meta))
-  {
-		add_action( 'wp_head', array(&$wpdanger_verification_meta, 'wpdanger_verification_show' ) );
-		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wpdanger_verification_settings_link' );
-		add_action( 'admin_init', 'wpdanger_verification_register_meta' );
-		add_action( 'admin_menu', 'wpdanger_verification_register_menu');
-		function wpdanger_verification_register_menu()
-    {
-      add_options_page(__('WPdanger Verification code', 'wpdanger-verification'), __('WPdanger Verification', 'wpdanger-verification'), 'manage_options', 'wpdanger_verification', 'wpdanger_verification_screenprint');	
+		if ( $ok )
+		{
+			echo '<meta name="wpdanger" content="' . wp_kses($verification_code, array(), array()) .'">' . "\n";
 		}
-		add_action( 'plugins_loaded', 'wpdanger_verification_textdomain' );	
-		function wpdanger_verification_textdomain() {
-			load_plugin_textdomain( 'wpdanger-verification', false, basename( dirname( __FILE__ ) ) . '/languages' );
-		}
+		unset($ok, $verification_code);
 	}
-	function wpdanger_verification_settings_link( $links )
-	{
-		$links[] = '<a href="' . get_admin_url( null, 'options-general.php?page=wpdanger_verification' ) . '">' . _('Settings') . '</a>';
-		return $links;
-	}
-	function wpdanger_verification_register_meta()
-	{
-    register_setting( 'wpdangerverification', 'wpdanger_verification_code', array('type' => 'string', 'default' => null) );
-	}
-	function wpdanger_verification_screenprint()
-	{
+}	
+
+add_action( 'wp_head', 'wpdanger_verification_show' );
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wpdanger_verification_settings_link' );
+add_action( 'admin_init', 'wpdanger_verification_register_meta' );
+add_action( 'admin_menu', 'wpdanger_verification_register_menu');
+function wpdanger_verification_register_menu()
+{
+	add_options_page(__('WPdanger Verification code', 'wpdanger-verification'), __('WPdanger Verification', 'wpdanger-verification'), 'manage_options', 'wpdanger_verification', 'wpdanger_verification_screenprint');	
+}
+add_action( 'plugins_loaded', 'wpdanger_verification_textdomain' );	
+function wpdanger_verification_textdomain() {
+	load_plugin_textdomain( 'wpdanger-verification', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+function wpdanger_verification_settings_link( $links )
+{
+	$links[] = '<a href="' . get_admin_url( null, 'options-general.php?page=wpdanger_verification' ) . '">' . _('Settings') . '</a>';
+	return $links;
+}
+function wpdanger_verification_register_meta()
+{
+	register_setting( 'wpdangerverification', 'wpdanger_verification_code', array('type' => 'string', 'default' => null) );
+}
+function wpdanger_verification_screenprint()
+{
 ?>
 		<div class="wrap">
       <h2><?php _e('WPdanger Verification', 'wpdanger-verification'); ?></h2>
@@ -91,5 +83,4 @@ if ( !class_exists('wpdanger_verification') )
       </form>
 		</div>
 <?php
-  }
 }
